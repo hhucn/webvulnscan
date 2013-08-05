c7848e35f67efd0384f2c11678e3f5c035d8dfc8
Richard Liebig <liebig.richard@hotmail.com>
2013-08-05 09:33:57 +0200

    utils.py: Removed unnecessary functions

--- src/utils.py
+++ src/utils.py
@@ -58,20 +58,3 @@ def get_page(url):
 
     return root 
 
-
-def print_msg(message):
-    """ Prints str(message) as a line.
-
-    """
-    stdout.write(str(message) + "\n")
-
-def print_vuln(vulnerability):
-    """ Should  print the vulnerability """
-    pass
-
-def print_err(error, dramatic=True):
-    """ Prints a error and exists if dramatic is set. """
-    stderr.write(str(error) + "\n")
-
-    if dramatic:
-        exit(2)
