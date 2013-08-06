372115bc5bacf9bf4ea84432fa322837b12b7be3
Richard Liebig <liebig.richard@hotmail.com>
2013-08-06 14:30:13 +0200

    crawling: Adjusted tests

--- test/test_crawling.py
+++ test/test_crawling.py
@@ -2,22 +2,23 @@ import unittest
 import xml.etree.ElementTree
 
 import tutil
-import webvulnscan
+import crawling
 
 
 class CrawlingTest(unittest.TestCase):
     def test_crawl(self):
         doc = xml.etree.ElementTree.fromstring(
             '<html><a href="/x">link</a><a name="here">here</a></html>')
-        self.assertEqual(list(webvulnscan.crawl(doc)), ['/x'])
+        self.assertEqual(list(crawling.links_on_site("http://test/", doc)),
+                         ['http://test/x'])
 
     def test_from_crawl(self):
         doc = xml.etree.ElementTree.fromstring(
             '<html><form action="/x">'
             '<input type="text" name="test"></input>'
             '</form></html>')
-        self.assertEqual(webvulnscan.form_crawl(doc),
-                         {"/x": {"test": None}})
+        values = {x:y for x, y in crawling.forms_on_site("http://test", doc)}
+        self.assertEqual(values, {"http://test/x": {"test": "text"}})
 
 if __name__ == '__main__':
     unittest.main()
