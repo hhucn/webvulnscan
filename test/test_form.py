75729f9aa738d1a64071f55351840522db9f5158
Richard Liebig <liebig.richard@hotmail.com>
2013-08-08 14:25:38 +0200

    test_form.py: Added testcases for the webvulnscan.Form class

---
+++ test/test_form.py
@@ -0,0 +1,55 @@
+import unittest
+import xml.etree.ElementTree as ET
+
+import tutil
+import webvulnscan.form
+
+
+class FormTest(unittest.TestCase):
+    def test_no_inputs_no_action(self):
+        doc = ET.fromstring('<form></form>')
+        form = webvulnscan.form.Form('http://test/', doc)
+        self.assertEqual({}, tutil.gen_to_dict(form.get_inputs()))
+        self.assertEqual("http://test/", form.action)
+
+    def test_no_inputs_with_Action(self):
+        doc = ET.fromstring('<form action="test"></form>')
+        form = webvulnscan.form.Form('http://test/', doc)
+        self.assertEqual(doc.items(), form.document.items())
+        self.assertEqual(doc.keys(), form.document.keys())
+        self.assertEqual("http://test/test", form.action)
+
+    def test_one_input_no_action(self):
+        doc = '<form><input type="text" name="test"></input></form>'
+        doc = ET.fromstring(doc)
+        form = webvulnscan.form.Form('http://test/', doc)
+        self.assertEqual({"test": "text"},
+                         tutil.gen_to_dict(form.get_inputs()))
+        self.assertEqual("http://test/", form.action)
+
+    def test_one_input_with_action(self):
+        doc = '<form action="test"><input type="text" name="test">' \
+              '</input></form>'
+        doc = ET.fromstring(doc)
+        form = webvulnscan.form.Form('http://test/', doc)
+        self.assertEqual({"test": "text"},
+                         tutil.gen_to_dict(form.get_inputs()))
+        self.assertEqual("http://test/test", form.action)
+
+    def test_serveral_inputs_no_action(self):
+        doc = '<form><input type="text" name="test"></input>' \
+              '<input type="submit" name="click"></input></form>'
+        doc = ET.fromstring(doc)
+        form = webvulnscan.form.Form('http://test/', doc)
+        self.assertEqual({"test": "text", "click": "submit"},
+                         tutil.gen_to_dict(form.get_inputs()))
+        self.assertEqual("http://test/", form.action)
+
+    def test_serveral_inputs_with_action(self):
+        doc = '<form action="action"><input type="text" name="test"> ' \
+              '</input><input type="submit" name="click"></input></form>'
+        doc = ET.fromstring(doc)
+        form = webvulnscan.form.Form('http://test/', doc)
+        self.assertEqual({"test": "text", "click": "submit"},
+                         tutil.gen_to_dict(form.get_inputs()))
+        self.assertEqual("http://test/action", form.action)
--- webvulnscan/form.py
+++ webvulnscan/form.py
@@ -4,16 +4,17 @@ except ImportError:
     from urlparse import urljoin
 
 
-def Form(object):
+class Form(object):
     def __init__(self, url, document):
         self.document = document
-        self.action = document.attrib.get('action')
+        self.action = urljoin(url, document.attrib.get('action'))
         self.parameters = {}
 
-    def inputs(self):
-        for link in self.document.findall('.//a[@href]'):
-            href = link.attrib.get('href')
-            yield urljoin(self.url, href)
+    def get_inputs(self):
+        for form_input in self.document.findall('.//input[@type]'):
+            form_input_type = form_input.attrib.get('type')
+            form_input_name = form_input.attrib.get('name')
+            yield form_input_name, form_input_type
 
     def send(self, client, parameters):
         return client.download_page(self.action, parameters)
