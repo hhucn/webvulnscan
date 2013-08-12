8f4fe5d9ad50db0614e0042f75cb0ee781318e33
Richard Liebig <liebig.richard@hotmail.com>
2013-08-12 08:56:43 +0200

    form.py: Added get_input_elements to Form object

--- webvulnscan/form.py
+++ webvulnscan/form.py
@@ -11,10 +11,14 @@ class Form(object):
         self.parameters = {}
 
     def get_inputs(self):
-        for form_input in self.document.findall('.//input[@type]'):
+        for form_input in self.get_input_elements():
             form_input_type = form_input.attrib.get('type')
             form_input_name = form_input.attrib.get('name')
             yield form_input_name, form_input_type
+
+    def get_input_elements(self):
+        for form_input in self.document.findall('.//input[@type]'):
+            yield form_input
 
     def send(self, client, parameters):
         return client.download_page(self.action, parameters)
