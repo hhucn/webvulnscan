e52eeebc919b0148e984ab1b56c1e6c1bb4db588
Richard Liebig <liebig.richard@hotmail.com>
2013-08-12 12:50:09 +0200

    form_input.py: Added more entrys to type_dictionary (fixes 12)

--- webvulnscan/form_input.py
+++ webvulnscan/form_input.py
@@ -1,12 +1,43 @@
 DEFAULT_TEXT_STRING = "abcdefgh"
 DEFAULT_EMAIL_STRING = "ex@amp.le"
+DEFAULT_PASSWORD_STRING = "abcd1234"
+DEFAULT_CHECKBOX_STRING = "checked"
+DEFAULT_RADIO_STRING = "1"
+DEFAULT_DATETIME_STRING = "1990-12-31T23:59:60Z"
+DEFAULT_DATETIME_LOCAL_STRING = "1985-04-12T23:20:50.52"
+DEFAULT_DATE_STRING = "1996-12-19"
+DEFAULT_MONTH_STRING = "1996-12"
+DEFAULT_TIME_STRING = "13:37:00"
+DEFAULT_WEEK_STRING = "1996-W16"
+DEFAULT_NUMBER_STRING = "123456"
+DEFAULT_RANGE_STRING = "1.23"
+DEFAULT_URL_STRING = "http://localhost/"
+DEFAULT_SEARCH_STRING = "query"
+DEFAULT_TEL_STRING = "012345678"
+DEFAULT_COLOR_STRING = "#FFFFFF"
 
 
 class FormInput(object):
     def __init__(self, element):
         self.element = element
         self.type_dictionary = {"text": DEFAULT_TEXT_STRING,
-                                "email": DEFAULT_EMAIL_STRING}
+                                "email": DEFAULT_EMAIL_STRING,
+                                "password": DEFAULT_PASSWORD_STRING,
+                                "checkbox": DEFAULT_CHECKBOX_STRING,
+                                "radio": DEFAULT_RADIO_STRING,
+                                "datetime": DEFAULT_DATETIME_STRING,
+                                "datetime-local":
+                                DEFAULT_DATETIME_LOCAL_STRING,
+                                "date": DEFAULT_DATE_STRING,
+                                "month": DEFAULT_MONTH_STRING,
+                                "time": DEFAULT_TIME_STRING,
+                                "week": DEFAULT_WEEK_STRING,
+                                "number": DEFAULT_NUMBER_STRING,
+                                "range": DEFAULT_RANGE_STRING,
+                                "url": DEFAULT_URL_STRING,
+                                "search": DEFAULT_SEARCH_STRING,
+                                "tel": DEFAULT_TEL_STRING,
+                                "color": DEFAULT_COLOR_STRING}
 
     def _get_attrib_value(self, name):
         value = self.element.attrib.get(name)
