aa69cd846a50eb967da48921988c4af89a3c34a6
Richard Liebig <liebig.richard@hotmail.com>
2013-08-14 14:27:19 +0200

    test_breach.py: Adjusted tests for new loggin framework

--- test/test_breach.py
+++ test/test_breach.py
@@ -1,6 +1,6 @@
 import tutil
 import unittest
-import random
+import sys
 import webvulnscan.attacks.breach
 from webvulnscan.page import Page
 
@@ -12,56 +12,52 @@ except ImportError:
 
 class BreachTest(unittest.TestCase):
     def test_static_site(self):
-        default_page = Page("/", "<html></html>", {}, 300)
+        default_page = Page("/", "<html></html>", {}, 200)
 
         class StaticSite(tutil.ClientSite):
             def download_page(self, url, parameters=None,
                               remember_visited=None):
                 return default_page
 
-        log_handler = tutil.LogHandler()
         my_attack = webvulnscan.attacks.breach.BreachAttack(default_page)
-        my_attack.log = log_handler
         my_attack.run(StaticSite())
 
-        self.assertEqual(len(log_handler.log_entrys), 0)
+        output = sys.stdout.getvalue().strip()
+        self.assertEqual(output, "")
 
     def test_activated_gzip(self):
         default_page = Page("/", "<html></html>", {"Content-Encoding": "GZIP"},
-                            300)
+                            200)
 
         class StaticSite(tutil.ClientSite):
             def download_page(self, url, parameters=None,
                               remember_visited=None):
                 return default_page
 
-        log_handler = tutil.LogHandler()
         my_attack = webvulnscan.attacks.breach.BreachAttack(default_page)
-        my_attack.log = log_handler
         my_attack.run(StaticSite())
 
-        self.assertEqual(len(log_handler.log_entrys), 1)
+        output = sys.stdout.getvalue().strip()
+        self.assertNotEqual(output, "")
 
     def test_breach_vulnerable(self):
-        token = [random.choice('01234567890ABCDEF') for x in range(8)]
-        token = ''.join(token)
+        token = tutil.random_string(8)
 
         form = '<form action="/?a=b"><input name="text" type="text" />' \
                + '<input name="token" type="hidden" value="' + token \
                + '" /></form>'
 
         default_page = Page("/?a=b", "<html>" + form + "</html>",
-                            {"Content-Encoding": "GZIP"}, 300)
+                            {"Content-Encoding": "GZIP"}, 200)
 
         class StaticSite(tutil.ClientSite):
             def download_page(self, url, parameters=None,
                               remember_visited=None):
                 return Page(url, "<html>" + form + unquote(url) + "</html>",
-                            {"Content-Encoding": "GZIP"}, 300)
+                            {"Content-Encoding": "GZIP"}, 200)
 
-        log_handler = tutil.LogHandler()
         my_attack = webvulnscan.attacks.breach.BreachAttack(default_page)
-        my_attack.log = log_handler
         my_attack.run(StaticSite())
 
-        self.assertEqual(len(log_handler.log_entrys), 1)
+        output = sys.stdout.getvalue().strip()
+        self.assertNotEqual(output, "")
