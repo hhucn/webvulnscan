1d6396f1de9d26d7aa30b14155a1af554fb4b850
Richard Liebig <liebig.richard@hotmail.com>
2013-08-15 09:31:07 +0200

    attacks/__init__.py: Added empty line for pep8

--- webvulnscan/attacks/__init__.py
+++ webvulnscan/attacks/__init__.py
@@ -3,21 +3,9 @@ from .xss import xss
 from .csrf import csrf
 from .breach import breach
 
+
 def AttackList():
     return [xss, csrf, breach]
-
-
-class AttackDriver(object):
-    def __init__(self, client):
-        self.attacks = []
-        self.client = client
-
-    def add_attack(self, attack):
-        self.attacks.append(attack)
-
-    def run(self, target_page):
-        for attack in self.attacks:
-            attack(target_page).run(self.client)
 
 
 def drive_all(page, attacks, client):
