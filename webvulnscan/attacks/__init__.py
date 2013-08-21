171cdacf854e926b6a92b530dfef94dc879875c9
Richard Liebig <liebig.richard@hotmail.com>
2013-08-21 08:51:26 +0200

    broken_unicode_chracters: Added testing for broken unicode chracters.

--- webvulnscan/attacks/__init__.py
+++ webvulnscan/attacks/__init__.py
@@ -5,10 +5,12 @@ from .breach import breach
 from .clickjack import clickjack
 from .cookiescan import cookiescan
 from .exotic_charaters import exotic_charaters
+from .broken_unicode_characters import broken_unicode_characters
 
 
 def AttackList():
-    return [xss, csrf, breach, clickjack, cookiescan, exotic_charaters]
+    return [xss, csrf, breach, clickjack, cookiescan, exotic_charaters,
+            broken_unicode_characters]
 
 
 def drive_all(page, attacks, client):
