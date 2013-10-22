8e69433b8afb9a702b75d59bb7a3da457631832c
Philipp Hagemeister <phihag@phihag.de>
2013-10-22 14:41:19 +0200

    Fix urlencoding on Python 2 / Python 3 (Fixes #62)

--- webvulnscan/request.py
+++ webvulnscan/request.py
@@ -1,4 +1,5 @@
 import copy
+import sys
 
 from . import compat
 from .compat import urlencode, parse_qs
@@ -10,10 +11,14 @@ class Request(compat.Request):
         if parameters is None:
             data = None
         else:
-            byte_parameters = dict(
-                (k.encode('utf-8'), v.encode('utf-8'))
-                for k, v in parameters.items())
-            data = urlencode(byte_parameters)
+            if sys.version_info >= (3, 0):
+                data = urlencode(parameters).encode('utf-8')
+            else:
+                byte_parameters = dict(
+                    (k.encode('utf-8'), v.encode('utf-8'))
+                    for k, v in parameters.items())
+                data = urlencode(byte_parameters)
+            assert isinstance(data, bytes)
         if headers is None:
             headers = {}
         compat.Request.__init__(self, url, data, headers)
