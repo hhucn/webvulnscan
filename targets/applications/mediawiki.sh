13b0b393c41fb7c01d30f530a4db3ad8309e0e76
Philipp Hagemeister <phihag@phihag.de>
2013-11-11 13:23:28 +0100

    Simplify mediawiki installation

--- targets/applications/mediawiki.sh
+++ targets/applications/mediawiki.sh
@@ -1,14 +1,11 @@
 MEDIAWIKI_WIKI_NAME="WebvulnWiki"
-MEDIAWIKI_INSTALL_FOLDER="mediawiki"
 MEDIAWIKI_SERVER="http://localhost"
 MEDIAWIKI_DATABASE="db_mediawiki"
 MEDIAWIKI_DATABASE_USER="usr_mediawiki"
 MEDIAWIKI_DATABASE_PASSWORD="mediawiki"
 
-rm -rf $APACHE_DIR/$MEDIAWIKI_INSTALL_FOLDER
-
-wget http://download.wikimedia.org/mediawiki/1.21/mediawiki-1.21.2.tar.gz -nv -O- | \
-	tar xfz - -C $APACHE_DIR --transform "s#^mediawiki-[0-9.]*/#$MEDIAWIKI_INSTALL_FOLDER/#"
+wget http://download.wikimedia.org/mediawiki/1.21/mediawiki-1.21.2.tar.gz -nv -O $TMPDIR/mediawiki.tar.gz -c
+tar xfz $TMPDIR/mediawiki.tar.gz -C $APACHE_DIR --transform "s#^mediawiki-[0-9.]*#mediawiki#"
 
 mysql -uroot -e \
 	"DROP DATABASE IF EXISTS $MEDIAWIKI_DATABASE;
@@ -16,16 +13,16 @@ mysql -uroot -e \
 	GRANT ALL PRIVILEGES ON "$MEDIAWIKI_DATABASE".* TO '$MEDIAWIKI_DATABASE_USER'@'localhost' IDENTIFIED BY '$MEDIAWIKI_DATABASE_PASSWORD';
 	FLUSH PRIVILEGES;"
 
-mysql -u$MEDIAWIKI_DATABASE_USER -p$MEDIAWIKI_DATABASE_PASSWORD $MEDIAWIKI_DATABASE < $APACHE_DIR/$MEDIAWIKI_INSTALL_FOLDER/maintenance/tables.sql
+mysql -u$MEDIAWIKI_DATABASE_USER -p$MEDIAWIKI_DATABASE_PASSWORD $MEDIAWIKI_DATABASE < $APACHE_DIR/mediawiki/maintenance/tables.sql
 
-cp $SCRIPTDIR/applications/mediawiki.config $APACHE_DIR/$MEDIAWIKI_INSTALL_FOLDER/LocalSettings.php
+cp $SCRIPTDIR/applications/mediawiki.config $APACHE_DIR/mediawiki/LocalSettings.php
 
-chown -R www-data:www-data $APACHE_DIR/$MEDIAWIKI_INSTALL_FOLDER
+chown -R www-data:www-data $APACHE_DIR/mediawiki
 
 sed -i -e 's#XXX_SITENAME_XXX#'$MEDIAWIKI_WIKI_NAME'#g' \
-       -e 's#XXX_SCRIPTPATH_XXX#'$MEDIAWIKI_INSTALL_FOLDER'#g' \
+       -e 's#XXX_SCRIPTPATH_XXX#'mediawiki'#g' \
        -e 's#XXX_SERVER_XXX#'$MEDIAWIKI_SERVER'#g' \
        -e 's#XXX_DBNAME_XXX#'$MEDIAWIKI_DATABASE'#g' \
        -e 's#XXX_DBUSER_XXX#'$MEDIAWIKI_DATABASE_USER'#g' \
        -e 's#XXX_DBPASS_XXX#'$MEDIAWIKI_DATABASE_PASSWORD'#g' \
-	$APACHE_DIR/$MEDIAWIKI_INSTALL_FOLDER/LocalSettings.php
+	$APACHE_DIR/mediawiki/LocalSettings.php
