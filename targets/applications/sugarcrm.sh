f7e8b89ce15cb1befcd553980838d7d59a6390ba
Virgil Grigoras <vigri100@uni-duesseldorf.de>
2013-11-25 12:13:25 +0100

    Quoting problem

--- targets/applications/sugarcrm.sh
+++ targets/applications/sugarcrm.sh
@@ -4,43 +4,43 @@ SUGAR_DB_NAME="db_sugarcrm"
 SUGAR_INSTALL_FOLDER="sugarcrm"
 SUGAR_VERSION="6.5.16"
 
-wget http://www.sugarforge.org/frs/download.php/10951/SugarCE-$SUGAR_VERSION.zip -O $TMPDIR/sugarcrm.zip -c
-unzip -qq $TMPDIR/sugarcrm.zip -d $INSTALL_DIR
-mv $INSTALL_DIR/SugarCE-Full* $INSTALL_DIR/$SUGAR_INSTALL_FOLDER
+#wget http://www.sugarforge.org/frs/download.php/10951/SugarCE-$SUGAR_VERSION.zip -O $TMPDIR/#sugarcrm.zip -c
+#unzip -qq $TMPDIR/sugarcrm.zip -d $INSTALL_DIR
+#mv $INSTALL_DIR/SugarCE-Full* $INSTALL_DIR/$SUGAR_INSTALL_FOLDER
 
 # Create config-file
-sudo sh -c "echo '<?php $sugar_config_si = array(
-'setup_db_host_name' => 'localhost',
-'setup_db_sugarsales_user' => '$SUGAR_DB_USER',
-'setup_db_sugarsales_password' => '$SUGAR_DB_USER_PASSWORD',
-'setup_db_database_name' => '$SUGAR_DB_NAME',
-'setup_db_type' => 'mysql',
-'setup_db_pop_demo_data' => false,
-'setup_db_create_database' => 1,
-'setup_db_create_sugarsales_user' => 1,
-'setup_db_admin_user_name' => 'root',
-'setup_db_admin_password' => '',
-'setup_db_drop_tables' => 0,
-'setup_db_username_is_privileged' => true,
-'dbUSRData' => 'create',
-'setup_site_url' => 'http://wvs.localhost/$SUGAR_INSTALL_FOLDER',
-'setup_site_admin_user_name'=>'admin',
-'setup_site_admin_password' => 'admin',
-'setup_site_sugarbeet_automatic_checks' => true, 
-'default_currency_iso4217' => 'EUR',
-'default_currency_name' => 'EUR Euro',
-'default_currency_significant_digits' => '2',
-'default_currency_symbol' => '$',
-'default_date_format' => 'Y-m-d',
-'default_time_format' => 'H:i',
-'default_decimal_seperator' => '.',
-'default_export_charset' => 'ISO-8859-1',
-'default_language' => 'en_us',
-'default_locale_name_format' => 's f l',
-'default_number_grouping_seperator' => ',',
-'export_delimiter' => ',',
-'setup_license_key' => 'LICENSE_KEY', 
-'setup_system_name' => 'SugarCRM - WVS Test',
+sudo sh -c "echo $'<?php '$sugar_config_si' = array(
+\'setup_db_host_name\' => \'localhost\',
+\'setup_db_sugarsales_user\' => \'$SUGAR_DB_USER\',
+\'setup_db_sugarsales_password\' => \'$SUGAR_DB_USER_PASSWORD\',
+\'setup_db_database_name\' => \'$SUGAR_DB_NAME\',
+\'setup_db_type\' => \'mysql\',
+\'setup_db_pop_demo_data\' => false,
+\'setup_db_create_database\' => 1,
+\'setup_db_create_sugarsales_user\' => 1,
+\'setup_db_admin_user_name\' => \'root\',
+\'setup_db_admin_password\' => \'\',
+\'setup_db_drop_tables\' => 0,
+\'setup_db_username_is_privileged\' => true,
+\'dbUSRData\' => \'create\',
+\'setup_site_url\' => \'http://wvs.localhost/$SUGAR_INSTALL_FOLDER\',
+\'setup_site_admin_user_name\'=>\'admin\',
+\'setup_site_admin_password\' => \'admin\',
+\'setup_site_sugarbeet_automatic_checks\' => true, 
+\'default_currency_iso4217\' => \'EUR\',
+\'default_currency_name\' => \'EUR Euro\',
+\'default_currency_significant_digits\' => \'2\',
+\'default_currency_symbol\' => \'$\',
+\'default_date_format\' => \'Y-m-d\',
+\'default_time_format\' => \'H:i\',
+\'default_decimal_seperator\' => \'.\',
+\'default_export_charset\' => \'ISO-8859-1\',
+\'default_language\' => \'en_us\',
+\'default_locale_name_format\' => \'s f l\',
+\'default_number_grouping_seperator\' => \',\',
+\'export_delimiter\' => \',\',
+\'setup_license_key\' => \'LICENSE_KEY\', 
+\'setup_system_name\' => \'SugarCRM - WVS Test\',
 ); ?>' >> /$INSTALL_DIR/$SUGAR_INSTALL_FOLDER/config_si.php"; 
 
 mysql -uroot -e "
@@ -52,6 +52,6 @@ mysql -uroot -e "
 
 cd $INSTALL_DIR/$SUGAR_INSTALL_FOLDER
 
-sudo chown www-data:www-data $INSTALL_DIR/* -R
+sudo chown www-data:www-data * -R
 
-sudo php -f install.php?goto=SilentInstall&cli=true
+#sudo php -f install.php?goto=SilentInstall&cli=true
