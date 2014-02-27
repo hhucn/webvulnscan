98af6d02cf28435d3c0344659c67394507d87dfe
Virgil Grigoras <vigri100@uni-duesseldorf.de>
2014-02-27 13:31:12 +0100

    vm config

--- targets/applications/adhocracy.sh
+++ targets/applications/adhocracy.sh
@@ -7,7 +7,6 @@ wget -nv https://raw.github.com/liqd/adhocracy/develop/build.sh -O $ADHOCRACY_DI
 cd $ADHOCRACY_DIR
 sh build.sh
 
-
 # Create a index.php file with a redirect to make adhocracy accessible via http://wvs.localhost/adhocracy/
 echo "<?php header( 'Location: http://localhost:5001' );?>" > index.php
 
