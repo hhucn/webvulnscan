402ec5c03f5152fb6904d9f20169f65d1bfd4006
Philipp Hagemeister <phihag@phihag.de>
2014-03-17 13:19:53 +0100

    Integrate wait_for_vm script into Makefile

--- vm/wait_for_vm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-MACHINE=$1
-echo "Waiting for machine $MACHINE to poweroff..."
-
-until $(VBoxManage showvminfo --machinereadable $MACHINE | grep -q ^VMState=.poweroff.)
-do
-  sleep 1
-done
