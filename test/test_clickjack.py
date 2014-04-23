b51dbb398344760e3f4cbdc5b84a57e7a3763b1e
Richard Liebig <liebig.richard@hotmail.com>
2014-04-23 12:01:22 +0200

    test/test_clickjack.py: Adjusted tests for webtest use

--- test/test_clickjack.py
+++ test/test_clickjack.py
@@ -5,33 +5,31 @@ import tutil
 import webvulnscan
 
 FORM_HTML = b'''<html>this is a form:
-                <form action="/delete" method="post">
+                <form action="./delete" method="post">
                     <input type="submit" />
                 </form>
                 </html>'''
 
 
 class ClickjackTest(unittest.TestCase):
-    def test_static_site(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': u'''<html>
-                <a href="/go">Links are (supposed to be) idempotent</a>
+                <a href="./go">Links are (supposed to be) idempotent</a>
                 </html>'''
-        })
+        }, [])
+    def test_static_site(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(0)
 
-    def test_get_form(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': u'''<html>
                 <form>
                     The default method is GET, so this should be fine
                     <input type="submit" />
                 </form>
                 </html>'''
-        })
+        }, [])
+    def test_clickjack_get_form(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(0)
 
         client = tutil.TestClient({
             '/': u'''<html>
@@ -44,60 +42,54 @@ class ClickjackTest(unittest.TestCase):
         client.run_attack(webvulnscan.attacks.clickjack)
         client.log.assert_count(0)
 
-    def test_vulnerable_site(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': (
                 200, FORM_HTML,
                 {'Content-Type': 'text/html; charset=utf-8'}),
-        })
+        }, ["Clickjacking"])
+    def test_clickjack_vulnerable_site(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(1)
 
-    def test_vulnerable_alternative_content_type(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': (
                 200, FORM_HTML,
                 {'Content-Type': 'application/xhtml+xml; charset=utf-8'}),
-        })
+        }, ["Clickjacking"])
+    def test_clickjack_vulnerable_alternative_content_type(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(1)
 
-    def test_secured_site(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': (
                 200, FORM_HTML,
                 {'Content-Type': 'text/html; charset=utf-8',
                  'X-Frame-Options': 'DENY'}),
-        })
+        }, [])
+    def test_clickjack_secured_site(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(0)
 
-    def test_sameorigin_site(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': (
                 200, FORM_HTML,
                 {'Content-Type': 'text/html; charset=utf-8',
                  'X-Frame-Options': 'SAMEORIGIN'}),
-        })
+        }, [])
+    def test_clickjack_sameorigin_site(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(0)
 
-    def test_allowfrom_site(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': (
                 200, FORM_HTML,
                 {'Content-Type': 'text/html; charset=utf-8',
                  'X-Frame-Options': 'ALLOW-FROM http://safe.example.org/'}),
-        })
+        }, [])
+    def test_clickjack_allowfrom_site(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(0)
 
-    def test_invalid_header(self):
-        client = tutil.TestClient({
+    @tutil.webtest({
             '/': (
                 200, FORM_HTML,
                 {'Content-Type': 'text/html; charset=utf-8',
                  'X-Frame-Options': 'None please!'}),
-        })
+        }, ["Clickjacking"])
+    def test_invalid_header(client):
         client.run_attack(webvulnscan.attacks.clickjack)
-        client.log.assert_count(1)
