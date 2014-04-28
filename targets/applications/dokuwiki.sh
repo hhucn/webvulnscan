480d19df86d90b1a0516cbe7bde3d7951d9b951e
Philipp Hagemeister <phihag@phihag.de>
2014-04-28 13:38:22 +0200

    [installscripts] Allow current user to write as well

--- targets/applications/dokuwiki.sh
+++ targets/applications/dokuwiki.sh
@@ -4,7 +4,8 @@ sudo rm -rf $TMP_DIR/dokuwiki
 wget http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz -nv -O $TMPDIR/dokuwiki-stable.tgz -c
 tar xfz $TMPDIR/dokuwiki-stable.tgz -C $INSTALL_DIR --transform "s#^dokuwiki-[0-9-]*#dokuwiki#"
 
-sudo chown www-data $INSTALL_DIR/dokuwiki/ -R
+sudo chown :www-data $INSTALL_DIR/dokuwiki/ -R
+sudo chmod g+rw $INSTALL_DIR/dokuwiki/ -R
 
 curl -c /tmp/cookieDokuWiki -b /tmp/cookieDokuWiki --globoff "http://wvs.localhost/dokuwiki/install.php" --data "l=en&d[acl]=on&d[title]=WebvulnWiki&d[superuser]=wvs&d[fullname]=wvs&d[email]=a@b.com&d[password]=wvs&d[confirm]=wvs&d[policy]=0&d[allowreg]=on&d[license]=cc-by-sa&d[pop]=on&submit=Save"
 sudo rm install.php
