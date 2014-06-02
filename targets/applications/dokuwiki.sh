12bfdd918b82799f123a19c3bf75ff2000db3bff
Virgil Grigoras <vigil.grigoras@uni-duesseldorf.de>
2014-06-02 14:35:38 +0200

    cookie-name is now randomized

--- targets/applications/dokuwiki.sh
+++ targets/applications/dokuwiki.sh
@@ -1,3 +1,8 @@
+DOKUWIKI_COOKIE=$(sudo mktemp $TMPDIR/XXXXXX)
+
+sudo chown user:user $DOKUWIKI_COOKIE
+
+
 sudo rm -rf $INSTALL_DIR/dokuwiki
 sudo rm -rf $TMP_DIR/dokuwiki
 
@@ -7,5 +12,5 @@ tar xfz $TMPDIR/dokuwiki-stable.tgz -C $INSTALL_DIR --transform "s#^dokuwiki-[0-
 sudo chown :www-data $INSTALL_DIR/dokuwiki/ -R
 sudo chmod g+rw $INSTALL_DIR/dokuwiki/ -R
 
-curl -c /tmp/cookieDokuWiki -b /tmp/cookieDokuWiki --globoff "http://wvs.localhost/dokuwiki/install.php" --data "l=en&d[acl]=on&d[title]=WebvulnWiki&d[superuser]=wvs&d[fullname]=wvs&d[email]=a@b.com&d[password]=wvs&d[confirm]=wvs&d[policy]=0&d[allowreg]=on&d[license]=cc-by-sa&d[pop]=on&submit=Save" > /dev/null
+curl -c $DOKUWIKI_COOKIE -b $DOKUWIKI_COOKIE --globoff "http://wvs.localhost/dokuwiki/install.php" --data "l=en&d[acl]=on&d[title]=WebvulnWiki&d[superuser]=wvs&d[fullname]=wvs&d[email]=a@b.com&d[password]=wvs&d[confirm]=wvs&d[policy]=0&d[allowreg]=on&d[license]=cc-by-sa&d[pop]=on&submit=Save" > /dev/null
 sudo rm -f install.php
