7cf9254e97e64ea210d12aa45162ed618b7e2ad8
Richard Liebig <liebig.richard@hotmail.com>
2014-07-07 12:41:35 +0200

    webvulnscan/form.py: Added detection if form is a search form

--- webvulnscan/form.py
+++ webvulnscan/form.py
@@ -15,6 +15,12 @@ class Form(object):
     def method(self):
         return self.document.attrib.get('method', 'get').lower()
 
+    @property
+    def is_search_form(self):
+        role = self.document.attrib.get('role', '').lower()
+        form_class = self.document.attrib.get('class', '').lower()
+        return role == "search" || form_class == "search"
+
     def get_inputs(self):
         for input_element in self.get_input_elements():
             yield FormInput(input_element)
