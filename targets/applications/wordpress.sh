bd65a0028c808db19016a93a0a22a88215f6bb63
Virgil Grigoras <vigil.grigoras@uni-duesseldorf.de>
2014-08-22 11:20:05 +0200

    Mechanism to detect a previous installation

--- targets/applications/wordpress.sh
+++ targets/applications/wordpress.sh
@@ -5,10 +5,16 @@ WORDPRESS_DATABASE_USER="usr_wordpress"
 WORDPRESS_ADMIN_USER="wordpress"
 WORDPRESS_ADMIN_PASSWORD="wordpress"
 
-cd $INSTALL_DIR
-sudo rm -rf $WORDPRESS_INSTALL_DIR
+if [ -d "$INSTALL_DIR/$WORDPRESS_INSTALL_DIR" ]; then
+    if [ "$OVERWRITE_EXISTING" = false ]; then
+    	printInfo "Skipping Wordpress installation: Wordpress is allready installed."
+    	return
+	fi
+fi
 
-wget http://wordpress.org/latest.tar.gz -O $TMPDIR/wordpress.tar.gz -c
+sudo rm -rf $INSTALL_DIR/$WORDPRESS_INSTALL_DIR
+
+download http://wordpress.org/latest.tar.gz wordpress.tar.gz
 tar xfz $TMPDIR/wordpress.tar.gz -C $INSTALL_DIR
 
 mysql -uroot -e "
@@ -23,39 +29,7 @@ sed -e "s#database_name_here#$WORDPRESS_DATABASE#g" \
     $INSTALL_DIR/$WORDPRESS_INSTALL_DIR/wp-config-sample.php \
     | tee $INSTALL_DIR/$WORDPRESS_INSTALL_DIR/wp-config.php >/dev/null
 
-echo "#!/usr/bin/php
-<?php
-// php-script used from http://stackoverflow.com/users/1437818/broesph and then modified
-
-function get_args(){
-        \$args = array();
-        for (\$i=1; \$i<count(\$_SERVER['argv']); \$i++){
-                \$arg = \$_SERVER['argv'][\$i];
-                if (\$arg{0} == '-' && \$arg{1} != '-'){
-                        for (\$j=1; \$j < strlen(\$arg); \$j++){
-                                \$key = \$arg{\$j};
-                                \$value = \$_SERVER['argv'][\$i+1]{0} != '-' ? preg_replace(array('/^[\"\\']/', '/[\"\']$/'), '', \$_SERVER['argv'][++\$i]) : true;
-                                \$args[\$key] = \$value;
-                        }
-                }
-                else
-                        \$args[] = \$arg;
-        }
-
-        return \$args;
-}
-
-// read commandline arguments
-\$opt = get_args();
-
-define( 'WP_INSTALLING', true );
- 
-require_once( dirname( dirname( __FILE__ ) ) . '/wp-load.php' );
-require_once( dirname( __FILE__ ) . '/includes/upgrade.php' );
-require_once(dirname(dirname(__FILE__)) . '/wp-includes/wp-db.php');
-
-\$result = wp_install(\$opt[0], \$opt[1], \$opt[2], false, '', \$opt[3]);
-" | sudo tee $INSTALL_DIR/$WORDPRESS_INSTALL_DIR/wp-admin/install.sh
+sudo mv $SCRIPTDIR/applications/wordpress_install.sh $INSTALL_DIR/$WORDPRESS_INSTALL_DIR/wp-admin/install.sh
 
 sudo chmod a+x $INSTALL_DIR/$WORDPRESS_INSTALL_DIR/wp-admin/install.sh
 
