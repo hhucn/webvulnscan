6622e3ac65d068be16bfcf0ce26328fc8a557ce1
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2014-09-03 21:59:49 +0200

    silent install

--- targets/applications/typo3.sh
+++ targets/applications/typo3.sh
@@ -33,19 +33,19 @@ mysql -uroot -e "
 
 
 # now we execute the web-installer
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?mode=123&step=1&password=joh316'    
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?mode=123&step=1&password=joh316'    
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?mode=123&step=1&password=joh316' > /dev/null
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?mode=123&step=1&password=joh316' > /dev/null
 
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=1' --data 'step=2' 
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=1' --data 'step=2' > /dev/null
 
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=2' --data 'step=3&TYPO3_INSTALL%5BLocalConfiguration%5D%5BencryptionKey%5D=39b70404ed0c893cd61f6ddb095b056614a3696379d3d6bce9c699bbb38f20ae762ff0fdbc363a854c4ee36d75985531&TYPO3_INSTALL%5BLocalConfiguration%5D%5Bcompat_version%5D=6.1&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_driver%5D=mysql&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_username%5D='$TYPO3_DATABASE_USER'&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_password%5D='$TYPO3_DATABASE_PASSWORD'&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_host%5D=localhost' 
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=3'   
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=2' --data 'step=3&TYPO3_INSTALL%5BLocalConfiguration%5D%5BencryptionKey%5D=39b70404ed0c893cd61f6ddb095b056614a3696379d3d6bce9c699bbb38f20ae762ff0fdbc363a854c4ee36d75985531&TYPO3_INSTALL%5BLocalConfiguration%5D%5Bcompat_version%5D=6.1&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_driver%5D=mysql&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_username%5D='$TYPO3_DATABASE_USER'&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_password%5D='$TYPO3_DATABASE_PASSWORD'&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db_host%5D=localhost' > /dev/null
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=3' > /dev/null
 
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=3' --data 'step=4&TYPO3_INSTALL%5Bdb_select_option%5D=EXISTING&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db%5D='$TYPO3_DATABASE
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=4'
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=config&mode=123&step=3' --data 'step=4&TYPO3_INSTALL%5Bdb_select_option%5D=EXISTING&TYPO3_INSTALL%5BDatabase%5D%5Btypo_db%5D='$TYPO3_DATABASE > /dev/null
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=4' > /dev/null
 
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=5' --data 'systemToInstall=Introduction&TYPO3_INSTALL%5Bdatabase_type%5D=import%7CCURRENT_TABLES%2BSTATIC&TYPO3_INSTALL%5Bdatabase_import_all%5D=1' 
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&systemToInstall=Introduction&step=5'      
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=5&subpackage=Introduction'      
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=5' --data 'systemToInstall=Introduction&TYPO3_INSTALL%5Bdatabase_type%5D=import%7CCURRENT_TABLES%2BSTATIC&TYPO3_INSTALL%5Bdatabase_import_all%5D=1' > /dev/null
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&systemToInstall=Introduction&step=5' > /dev/null
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=5&subpackage=Introduction' > /dev/null
 
-curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=6' --data 'password='$TYPO3_ADMIN_PASSWORD'&useRealURL=1&colorPicker=%23F18F0B' 
+curl -c $TYPO3_COOKIE -b $TYPO3_COOKIE --globoff 'http://wvs.localhost/typo3/typo3/install/index.php?TYPO3_INSTALL[type]=database&mode=123&step=6' --data 'password='$TYPO3_ADMIN_PASSWORD'&useRealURL=1&colorPicker=%23F18F0B' > /dev/null
