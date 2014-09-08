e1b5a6b22320069d8d54666926182e6a5de4c210
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2014-09-08 10:34:41 +0200

    intermediate step

--- targets/applications/ejbca.sh
+++ targets/applications/ejbca.sh
@@ -3,6 +3,8 @@ EJBCA_DATABASE_USER="usr_ejbca"
 EJBCA_DATABASE_PASSWORD="ejbca"
 EJBCA_DIR="$INSTALL_DIR/ejbca_ce_6_2_0"
 JBOSS_DIR="$INSTALL_DIR/JBOSS_7_1_1"
+JBOSS_VERSION_MINOR="7.1.1"
+JBOSS_VERSION_MAJOR="7.1"
 MYSQL_CONNECTOR_DIR="$INSTALL_DIR/mysqlConnector"
 
 # Create User and Group
@@ -24,32 +26,30 @@ sudo chown jboss:jboss /var/log/ejbca
 #sudo rm -f $TMPDIR/EJBCA*
 sudo rm -rf $EJBCA_DIR
 sudo rm -rf $JBOSS_DIR
+sudo rm -rf /opt/ejbca
+sudo rm -rf /opt/jboss
+sudo rm -rf /etc/ejbca
 
-#mkdir $JBOSS_DIR
-cd $INSTALL_DIR
-
+# create folders and links
 mkdir -p $JBOSS_DIR
 mkdir -p $INSTALL_DIR/"$MYSQL_CONNECTOR_DIR"
+sudo mkdir /etc/ejbca
+sudo mkdir -p /var/log/ejbca
+sudo chown jboss:jboss /var/log/ejbca
 
-# create symbolic links
-if [[ ! -h "/opt/ejbca" ]]; then 
-	sudo ln -s $EJBCA_DIR /opt/ejbca
-fi
+sudo ln -s $EJBCA_DIR /opt/ejbca
+sudo ln -s $JBOSS_DIR /opt/jboss
 
-if [[ ! -h "/opt/jboss" ]]; then 
-	sudo ln -s $JBOSS_DIR /opt/jboss
-fi
 
 # get ejbca and move it to "installed"
 download "http://downloads.sourceforge.net/project/ejbca/ejbca6/ejbca_6_2_0/ejbca_ce_6_2_0.zip?r=&ts=$(timestamp)&use_mirror=optimate" EJBCA.zip
-unzip -o $TMPDIR/EJBCA.zip -d $INSTALL_DIR
-#mv $TMPDIR/ejbca*/* $INSTALL_DIR/ejbca/
+unzip -qq -o $TMPDIR/EJBCA.zip -d $INSTALL_DIR
 
-#return
-# get JBOSS AS 7.1.1
-download http://download.jboss.org/jbossas/7.1/jboss-as-7.1.1.Final/jboss-as-7.1.1.Final.zip jboss.zip
-unzip -o $TMPDIR/jboss.zip -d $INSTALL_DIR
-mv $INSTALL_DIR/jboss-as* $JBOSS_DIR
+# get JBOSS AS
+download http://download.jboss.org/jbossas/$JBOSS_VERSION_MAJOR/jboss-as-$JBOSS_VERSION_MINOR.Final/jboss-as-$JBOSS_VERSION_MINOR.Final.zip jboss.zip
+unzip -qq -o $TMPDIR/jboss.zip -d $INSTALL_DIR
+mv $INSTALL_DIR/jboss-as-$JBOSS_VERSION_MINOR.Final/* $JBOSS_DIR/
+rm -rf $INSTALL_DIR/jboss-as-$JBOSS_VERSION_MINOR.Final
 
 # get mysql-connector 5.1.30
 download http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.30/mysql-connector-java-5.1.30.jar mysql-connector-java-5.1.30.jar
@@ -62,11 +62,66 @@ mysql -uroot -e \
 	GRANT ALL PRIVILEGES ON "$EJBCA_DATABASE".* TO '$EJBCA_DATABASE_USER'@'localhost' IDENTIFIED BY '$EJBCA_DATABASE_PASSWORD';
 	FLUSH PRIVILEGES;"
 
+
 cd /opt/jboss/bin
 cp standalone.conf standalone.conf.orig
+	
+sed -i -e 's#\#JAVA_HOME="/opt/java/jdk"#'JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/"'#g' \
+	standalone.conf
 
-cp /opt/jboss/bin/init.d/jboss-as-standalone.sh /etc/init.d/ejbca
-mkdir /etc/ejbca
-cp /opt/jboss/bin/init.d/jboss-as.conf /etc/ejbca/ejbca-init.conf
+# prevent "success/failure: command not found - error in debian"
+sed -e 's#success#$echo "[SUCCESS]"/#g' \
+    -e 's#failure#$echo "[FAILURE]"#g' \
+    $SCRIPTDIR/applications/ejbca_config/ejbca_init_script.sh \
+    | sudo tee /etc/init.d/ejbca >/dev/null
+
+sudo chmod a+x /etc/init.d/ejbca
+
+sudo cp $SCRIPTDIR/applications/ejbca_config/ejbca_init_conf.conf /etc/ejbca/ejbca-init.conf
+
+# ports 8080, 8443... are in use... we need to change them and configure the mysql connector
+sed -i -e 's#8080#7080#g' \
+		-e 's#8443#7443#g' \
+		-e 's#8090#7090#g' \
+		-e 's#9999#7999#g' \
+		-e 's#9990#7990#g' \
+		-e 's#9443#7443#g' \
+		-e 's#8009#7009#g' \
+		-e 's#<driver name="h2" module="com.h2database.h2">#<driver name="com.mysql.jdbc.Driver" module="com.mysql">#g' \
+		-e 's#<xa-datasource-class>org.h2.jdbcx.JdbcDataSource</xa-datasource-class>#<xa-datasource-class>com.mysql.jdbc.jdbc.jdbc2.optional.MysqlXADataSource</xa-datasource-class>#g' \
+	$JBOSS_DIR/standalone/configuration/standalone.xml
+
+#sed -i -e 's#8080#7080#g' \
+#		-e 's#8443#7443#g' \
+#		-e 's#8090#7090#g' \
+#		-e 's#9999#7999#g' \
+#		-e 's#9990#7990#g' \
+#		-e 's#9443#7443#g' \
+#		-e 's#8009#7009#g' \
+#	$JBOSS_DIR/standalone/configuration/standalone-full.xml
+
+sed -i -e 's#9999#7999#g' \
+	$JBOSS_DIR/bin/jboss-cli.xml
 
 
+sed -i -e 's#</paths>#<path name="sun/security/x509"/><path name="sun/security/pkcs11"/><path name="sun/security/pkcs11/wrapper"/><path name="sun/security/action"/></paths>#g' \
+	/opt/jboss/modules/sun/jdk/main/module.xml
+
+mkdir -p /opt/jboss/modules/com/mysql/main/
+cd /opt/jboss/modules/com/mysql/main
+ln -s $MYSQL_CONNECTOR_DIR/mysql-connector-java-5.1.30.jar mysql-connector-java.jar
+
+cp $SCRIPTDIR/applications/ejbca_config/jboss_mysql_connector_module module.xml
+
+sudo chown -R jboss:jboss $JBOSS_DIR
+
+#cd /opt/jboss/bin
+#sudo sh jboss-cli.sh <<!
+#connect
+#/subsystem=datasources/jdbc-driver=com.mysql.jdbc.Driver:add(driver-name=com.mysql.jdbc.Driver,driver-module-name=com.mysql,driver-xa-datasource-class-name=com.mysql.jdbc.jdbc.jdbc2.optional.MysqlXADataSource)
+#:reload
+#exit
+#!
+
+
+echo "done"
