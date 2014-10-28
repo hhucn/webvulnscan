7539bf2efad70d3b809d2a3a64c76f3792cf0767
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2014-10-29 00:31:58 +0100

    Intermediate step

--- vm/init-wvsvm.sh
+++ vm/init-wvsvm.sh
@@ -2,47 +2,29 @@
 
 set -e
 
-aptitude clean && aptitude update && aptitude dist-upgrade -y && aptitude clean && \
-aptitude install -y \
-	build-essential gdb unzip zip mercurial htop \
-	linux-headers-2.6.24-32 linux-headers-2.6.24-32-generic \
-    dkms \
-	nasm dosbox \
-	xfonts-base xkb-data xserver-xorg-video-vesa xserver-xorg xserver-xorg-input-mouse xserver-xorg-input-synaptics xserver-xorg-input-kbd \
-	xfwm4 xfdesktop4 xfce4-taskmanager xfce4-session xfce4-taskmanager xfce4-panel xfce4-utils xfce4-appfinder xfce4-battery-plugin \
-	tango-icon-theme tango-icon-theme-common xcursor-themes xfwm4-themes  \
-	gtk2-engines-xfce xfce4-icon-theme \
-	gdm gdm-themes xterm xarchiver thunar xfce4-terminal geany evince \
-	nmap
-aptitude clean
+#grep -q wvsvm /etc/sudoers || /bin/echo -e '\n\nwebvulnscan ALL=(ALL) NOPASSWD: ALL\n' >> /etc/sudoers
 
-sed -i 's#^timeout\s\{0,\}3#timeout 0#' /boot/grub/menu.lst
-grep -q wvsvm /etc/sudoers || /bin/echo -e '\n\nwvsvm ALL=(ALL) NOPASSWD: ALL\n' >> /etc/sudoers
-grep -q wvsvm /etc/gdm/gdm.conf || ( \
-	sed -i 's#\(AutomaticLoginEnable=\)false#\1true#' /etc/gdm/gdm.conf && \
-	sed -i 's#\(AutomaticLogin=\)#\1wvsvm#' /etc/gdm/gdm.conf \
-)
+sudo chown -R webvulnscan:webvulnscan /wvsvm-init
 
+sudo mkdir -p /mnt/VBoxISO
+sudo mount -o loop /wvsvm-init/VBoxGuestAdditions_4.3.6.iso /mnt/VBoxISO
 
-chmod a+x /wvsvm-init/vbox-guestutils.sh
-if ! /wvsvm-init/vbox-guestutils.sh; then
-	echo "Installation of VirtualBox guest additions FAILED!"
-	exit 1
-fi
+sh /mnt/VBoxISO/VBoxLinuxAdditions.run --nox11 <<!
+yes
+!
 
-mkdir -p /home/wvsvm/Desktop
+sudo umount /mnt/VBoxISO
 
-
+#mkdir -p /home/wvsvm/Desktop
 
 #cp /wvsvm-init/aufgaben.tar.bz2 /home/wvsvm/
 #tar -C /home/wvsvm/ -x -f /wvsvm-init/aufgaben.tar.bz2
 #tar -C /home/wvsvm/ -x -f /wvsvm-init/xfce-config.tar.bz2
 
+#echo -e 'Username: webvulnscan\nPassword: 123456\nroot with sudo -s\n' > /home/wvsvm/Desktop/password
+chown -R webvulnscan:webvulnscan /home/webvulnscan/
 
-echo -e 'Username: wvsvm\nPassword: 123456\nroot with sudo -s\n' > /home/wvsvm/Desktop/password
-chown -R wvsvm:wvsvm /home/wvsvm/
-
-aptitude purge -y linux-image-2.6.24-26-generic linux-restricted-modules-2.6.24-26-generic linux-ubuntu-modules-2.6.24-26-generic linux-headers-2.6.24-16
+#aptitude purge -y linux-image-2.6.24-26-generic linux-restricted-modules-2.6.24-26-generic linux-ubuntu-modules-2.6.24-26-generic linux-headers-2.6.24-16
 
 #find /wvsvm-init -type f -exec shred -n 0 -z --remove '{}' ';'
 #rm -rf /wvsvm-init
