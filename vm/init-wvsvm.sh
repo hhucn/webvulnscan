31dfe44e0f7a35106aaedaffae283f0bf856352e
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2014-10-29 11:34:04 +0100

    Intermediate step

--- vm/init-wvsvm.sh
+++ vm/init-wvsvm.sh
@@ -15,18 +15,12 @@ yes
 
 sudo umount /mnt/VBoxISO
 
-#mkdir -p /home/wvsvm/Desktop
-
-#cp /wvsvm-init/aufgaben.tar.bz2 /home/wvsvm/
-#tar -C /home/wvsvm/ -x -f /wvsvm-init/aufgaben.tar.bz2
-#tar -C /home/wvsvm/ -x -f /wvsvm-init/xfce-config.tar.bz2
-
-#echo -e 'Username: webvulnscan\nPassword: 123456\nroot with sudo -s\n' > /home/wvsvm/Desktop/password
-chown -R webvulnscan:webvulnscan /home/webvulnscan/
-
-#aptitude purge -y linux-image-2.6.24-26-generic linux-restricted-modules-2.6.24-26-generic linux-ubuntu-modules-2.6.24-26-generic linux-headers-2.6.24-16
-
-#find /wvsvm-init -type f -exec shred -n 0 -z --remove '{}' ';'
-#rm -rf /wvsvm-init
+mkdir -p /home/webvulnscan/Desktop/
+cd /home/webvulnscan/Desktop/
+git clone https://github.com/hhucn/webvulnscan.git
+cd webvulnscan
+git checkout installscripts
+cd targets
+./install.sh
 
 echo INSTALLATION successful
