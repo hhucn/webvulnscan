e358e6ea3920ca9b7c0f5ab3eede44becb53dc40
Virgil Grigoras <virgil.grigoras@uni-duesseldorf.de>
2014-10-31 09:53:52 +0100

    added loging

--- vm/init-wvsvm.sh
+++ vm/init-wvsvm.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
+DEBUG=true
+logfile=/var/log/webvulnscan.log
+
+exec > >(gawk -v pid=$$ '{ print strftime("%F-%T"),pid,$0; fflush(); }' | tee -a $logfile)
+    [ ! -z "$DEBUG" ] && exec 2>&1 || exec 2> >(gawk -v pid=$$ '{ print strftime("%F-%T"),pid,$0; fflush(); }' >>$logfile)
+    echo "=== Log started for $$ at $(date +%F-%T) ==="
+
 set -e
 
 #grep -q wvsvm /etc/sudoers || /bin/echo -e '\n\nwebvulnscan ALL=(ALL) NOPASSWD: ALL\n' >> /etc/sudoers
+# Update System
+sudo apt-get -y update > /dev/null 2>&1
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -qqy install git-core htop axel unzip
 
 sudo chown -R webvulnscan:webvulnscan /wvsvm-init
 
 sudo mkdir -p /mnt/VBoxISO
-sudo mount -o loop /wvsvm-init/VBoxGuestAdditions_4.3.6.iso /mnt/VBoxISO
 
-sh /mnt/VBoxISO/VBoxLinuxAdditions.run --nox11 <<!
+mounted=$(mount | grep "VBoxGuestAdditions")
+if [ "$mounted" = "" ]; then
+	sudo mount -o loop /wvsvm-init/VBoxGuestAdditions_4.3.6.iso /mnt/VBoxISO
+fi
+
+sudo sh /mnt/VBoxISO/VBoxLinuxAdditions.run --nox11 <<!
 yes
 !
 
@@ -24,3 +39,6 @@ cd targets
 ./install.sh
 
 echo INSTALLATION successful
+function startLogging {
+    
+}
