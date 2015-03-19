3c2e2fc470e53cc224295062867cd989d33ed378
Philipp Hagemeister <phihag@phihag.de>
2015-03-19 16:50:40 +0100

    Update check_port_free

--- targets/check_port_free.py
+++ targets/check_port_free.py
@@ -3,25 +3,28 @@
 
 from __future__ import print_function
 
+import errno
+import glob
 import os
+import re
+import signal
 import socket
 import struct
-import glob
-import re
 import sys
 import time
-import signal
 
 _STATE_LISTEN = '0A'
 
 _NETSTAT_FILES_TCP = ['/proc/net/tcp', '/proc/net/tcp6']
 
-def _signalByName(name):
+
+def _signal_by_name(name):
     try:
         return int(name)
     except ValueError:
         pass
-    signames = dict((k, v) for k,v in signal.__dict__.items() if k.startswith('SIG'))
+    signames = dict(
+        (k, v) for k, v in signal.__dict__.items() if k.startswith('SIG'))
     if name in signames:
         return signames[name]
     sname = 'SIG_' + name
@@ -32,49 +35,54 @@ def _signalByName(name):
         return signames[sname]
     raise ValueError('Cannot find signal ' + str(name))
 
-def _parseIpPort(kernelStr):
-    ipStr,_,portStr = kernelStr.partition(':')
-    port = int(portStr, 16)
-    ipInt = int(ipStr, 16)
-    if len(ipStr) == 8: # IPv4
-        ip = socket.inet_ntop(socket.AF_INET, struct.pack('=I', ipInt))
-    else: # IPv6
-        ip = socket.inet_ntop(socket.AF_INET6, struct.pack('=IIII',
-            ((ipInt >> 96) & 0xffffffff),
-            ((ipInt >> 64) & 0xffffffff),
-            ((ipInt >> 32) & 0xffffffff),
-            ((ipInt >> 0) & 0xffffffff)
-        ))
-    return ip,port
 
-class NoProcessException(Exception):
+def _parse_ip_port(kernel_str):
+    ipStr, _, portStr = kernel_str.partition(':')
+    port = int(portStr, 16)
+    ip_int = int(ipStr, 16)
+    if len(ipStr) == 8:  # IPv4
+        ip = socket.inet_ntop(socket.AF_INET, struct.pack('=I', ip_int))
+    else:  # IPv6
+        ip = socket.inet_ntop(socket.AF_INET6, struct.pack(
+            '=IIII',
+            ((ip_int >> 96) & 0xffffffff),
+            ((ip_int >> 64) & 0xffffffff),
+            ((ip_int >> 32) & 0xffffffff),
+            ((ip_int >> 0) & 0xffffffff)
+        ))
+    return (ip, port)
+
+
+class NoProcessException(BaseException):
     def __init__(self, msg):
         super(NoProcessException, self).__init__()
         self.msg = msg
 
-def _get_open_ports(sourceFiles=_NETSTAT_FILES_TCP):
-    for sfn in sourceFiles:
+
+def _get_open_ports(source_files=_NETSTAT_FILES_TCP):
+    for sfn in source_files:
         with open(sfn) as sf:
             proto = os.path.basename(sfn)
-            next(sf) # Skip header line
+            next(sf)  # Skip header line
 
             for line in sf:
                 entries = line.split()
                 state = entries[3]
-                sockId = entries[9]
+                sock_id = entries[9]
                 if state != _STATE_LISTEN:
                     continue
 
-                local_ip,local_port = _parseIpPort(entries[1])
+                local_ip, local_port = _parse_ip_port(entries[1])
 
                 yield {
                     'proto': proto,
                     'port': local_port,
                     'local_address': local_ip,
-                    'sockId': sockId,
+                    'sock_id': sock_id,
                 }
 
-def netstat(includePrograms=True, sourceFiles=_NETSTAT_FILES_TCP):
+
+def netstat(include_programs=True, source_files=_NETSTAT_FILES_TCP):
     """ Returns a list of dictionaries with the following properties:
         proto: The procotol name ('tcp' or 'tcp6')
         port: The port number the application is listening on
@@ -82,20 +90,20 @@ def netstat(includePrograms=True, sourceFiles=_NETSTAT_FILES_TCP):
         pgid: The process group ID
         exe: executable name of the program in question
     """
-    res = list(_get_open_ports(sourceFiles))
-    if includePrograms:
+    res = list(_get_open_ports(source_files))
+    if include_programs:
         for fdFile in glob.iglob('/proc/[0-9]*/fd/*'):
             pid = int(fdFile.split('/')[2])
             try:
-                linkTarget = os.readlink(fdFile)
+                link_target = os.readlink(fdFile)
             except OSError:
                 continue
 
-            m = re.match('^socket:\[(?P<sockId>[0-9]+)\]$', linkTarget)
+            m = re.match('^socket:\[(?P<sock_id>[0-9]+)\]$', link_target)
             if m is not None:
-                sockId = m.group('sockId')
+                sock_id = m.group('sock_id')
                 for d in res:
-                    if d['sockId'] == sockId:
+                    if d['sock_id'] == sock_id:
                         d['pid'] = pid
                         try:
                             d['exe'] = os.readlink('/proc/' + str(pid) + '/exe')
@@ -109,18 +117,20 @@ def netstat(includePrograms=True, sourceFiles=_NETSTAT_FILES_TCP):
 
     return res
 
-def checkOnce(ports, opts_open=False, opts_kill='dont', opts_killSignal=signal.SIGTERM):
+
+def check_once(
+        ports, opts_open=False, opts_kill='dont', opts_kill_signal=signal.SIGTERM):
     nstat = netstat()
     errors = []
     messages = []
     for p in ports:
-        matchingDicts = list(filter(lambda d: d['port'] == p, nstat))
-        if len(matchingDicts) == 0:
+        matching_dicts = list(filter(lambda d: d['port'] == p, nstat))
+        if not matching_dicts:
             if opts_open:
                 errors.append('Port %s not taken by any program' % p)
         else:
             if not opts_open:
-                d = matchingDicts[0]
+                d = matching_dicts[0]
 
                 # Recheck if that's still the case;
                 # the port may have been closed since we last checkd
@@ -136,7 +146,7 @@ def checkOnce(ports, opts_open=False, opts_kill='dont', opts_killSignal=signal.S
                         raise NoProcessException('Cannot find process occupying port %s - are you root?' % d['port'])
                     messages.append('Killing pid ' + str(d['pid']) + ' (' + bexename + ' bound to ' + d['local_address'] + ':' + str(d['port']) + ')')
                     try:
-                        os.kill(d['pid'], opts_killSignal)
+                        os.kill(d['pid'], opts_kill_signal)
                     except OSError as ose:
                         if ose.errno != errno.ESRCH:
                             raise
@@ -145,18 +155,23 @@ def checkOnce(ports, opts_open=False, opts_kill='dont', opts_killSignal=signal.S
                         raise NoProcessException('Cannot find process group occupying port %s - are you root?' % d['port'])
                     messages.append('Killing pgid ' + str(d['pgid']) + ' (' + bexename + ' bound to ' + d['local_address'] + ':' + str(d['port']) + ')')
                     try:
-                        os.killpg(d['pgid'], opts_killSignal)
+                        os.killpg(d['pgid'], opts_kill_signal)
                     except OSError as ose:
                         if ose.errno != errno.ESRCH:
                             raise
-    return messages,errors
+    return (messages, errors)
 
-def check_port_free(ports, message_printer=None, opts_gracePeriod=0, opts_graceInterval=1, opts_open=False, opts_kill='dont', opts_killSignal=signal.SIGTERM):
+
+def check_port_free(
+        ports, message_printer=None,
+        opts_grace_period=0, opts_grace_interval=1, opts_open=False,
+        opts_kill='dont', opts_kill_signal=signal.SIGTERM):
     if message_printer is None:
-        message_printer = lambda messages: None
-    remaining_grace = opts_gracePeriod
+        def message_printer(messages):
+            pass
+    remaining_grace = opts_grace_period
     while True:
-        messages,errors = checkOnce(ports, opts_open, opts_kill, opts_killSignal)
+        messages, errors = check_once(ports, opts_open, opts_kill, opts_kill_signal)
         if messages:
             message_printer(messages)
         if not errors or remaining_grace <= 0:
@@ -164,9 +179,9 @@ def check_port_free(ports, message_printer=None, opts_gracePeriod=0, opts_graceI
         if messages:
             message_printer(messages)
 
-        sleepTime = min(remaining_grace, opts_graceInterval)
-        time.sleep(sleepTime)
-        remaining_grace -= sleepTime
+        sleep_time = min(remaining_grace, opts_grace_interval)
+        time.sleep(sleep_time)
+        remaining_grace -= sleep_time
     if errors:
         message_printer(errors)
     return errors
@@ -189,29 +204,33 @@ def main():
         help='kill the offending process groups')
     parser.add_option(
         '--kill-signal',
-        dest='killSignalStr', default=signal.SIGTERM,
+        dest='kill_signal_str', default=signal.SIGTERM,
         help='Use the specified signal instead of SIGTERM', metavar='SIGNAL')
     parser.add_option(
         '-g', '--grace-period',
-        dest='gracePeriod', type='float', default=0, metavar='SECONDS',
+        dest='grace_period', type='float', default=0, metavar='SECONDS',
         help='Seconds to wait for the condition to be fulfilled')
-    parser.add_option('--grace-interval',
-        dest='graceInterval', type='float', default=1, metavar='SECONDS',
-        help='Check every n seconds')
+    parser.add_option(
+        '--grace-interval',
+        dest='grace_interval', type='float', default=0.5, metavar='SECONDS',
+        help='Check every n seconds (default: %default).')
     (opts, args) = parser.parse_args()
 
     ports = list(map(int, args))
     if not ports:
         parser.error('Need at least one port to test')
-    opts.killSignal = _signalByName(opts.killSignalStr)
-    message_printer = lambda messages: print('\n'.join(messages))
+    opts.kill_signal = _signal_by_name(opts.kill_signal_str)
+
+    def message_printer(messages):
+        print('\n'.join(messages))
 
     try:
-        errors = check_port_free(ports, message_printer,
-            opts_gracePeriod=opts.gracePeriod,
-            opts_graceInterval=opts.graceInterval,
+        errors = check_port_free(
+            ports, message_printer,
+            opts_grace_period=opts.grace_period,
+            opts_grace_interval=opts.grace_interval,
             opts_open=opts.open, opts_kill=opts.kill,
-            opts_killSignal=opts.killSignal)
+            opts_kill_signal=opts.kill_signal)
     except NoProcessException as ne:
         sys.stderr.write(ne.msg + '\n')
         sys.exit(4)
@@ -219,4 +238,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
